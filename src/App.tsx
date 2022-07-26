aa183edffa03ae1bd5aa6ba66e24df72ace54546
Bugra Akdogan <akdogan.bugra@gmail.com>
2022-07-26 12:20:15 -0700

    Added patientsSlice and separated components

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,15 @@
-import { useEffect, useState } from "react";
-import { CclReturnData, getUserData } from "./data";
-import { User } from "./data";
+import LoggedIn from "./pages/LoggedIn";
 import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "./redux/slices/userSlice";
 import { RootState } from "./redux/store";
+import SignIn from "./pages/SignIn";
+import { Route, Routes, Link } from "react-router-dom";
+import { reset } from "./redux/slices/userSlice";
 
 export default () => {
-  // const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const dispatch = useDispatch();
-  const user = useSelector((state: RootState) => state.user);
+  // const user = useSelector((state: RootState) => state.user.name);
+  // const dispatch = useDispatch();
+  // dispatch(reset);
+  // return user ? <LoggedIn /> : <SignIn />;
 
-  useEffect(() => {
-    setLoading(true);
-    getUserData(1)
-      .then((result) => {
-        const { error, msg } = validateGetUser(result);
-        if (error) {
-          console.error("No Result");
-          return;
-        } else {
-          // setUser(result.DATA[0]);
-          dispatch(setUser(result.DATA[0]));
-          // console.log(result.DATA[0]);
-        }
-      })
-      .then((err) => console.error(err))
-      .finally(() => setLoading(false));
-  }, []);
-
-  if (loading) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <p>{JSON.stringify(user)}</p>
-    </div>
-  );
+  return <LoggedIn />;
 };
-
-function validateGetUser(res: CclReturnData<User>): {
-  error: boolean;
-  msg?: string;
-} {
-  // console.log(res.META);
-  if (!res.DATA) {
-    return { error: true, msg: "no valid data was found for the user" };
-  }
-  if (res.DATA.length === 0) {
-    return { error: true, msg: "there was no data for the user" };
-  }
-  if (res.DATA.length > 1) {
-    return {
-      error: true,
-      msg: "more than one user was found; was expecting only one",
-    };
-  }
-  return { error: false };
-}
