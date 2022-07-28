d88aae91688bcf88eede9bc5b548b8875ceb77d6
Bugra Akdogan <akdogan.bugra@gmail.com>
2022-07-27 20:00:12 -0700

    patient data changes

--- src/pages/PatientLabs.tsx
+++ src/pages/PatientLabs.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import { CclReturnData, getPatientLabs, Lab } from "../data";
 
-type Props = {};
+function PatientLabs() {
+  getPatientLabs(1)
+    .then((result) => {
+      const { error, msg } = validateGetPatientLabs(result);
+      if (error) {
+        console.error("No Result");
+        return;
+      } else {
+        // setUser(result.DATA[0]);
+        return { result };
+        // console.log(result.DATA[0]);
+      }
+    })
+    .then((err) => console.error(err));
+}
 
-function PatientLabs({}: Props) {
-  return <div>PatientLabs</div>;
+function validateGetPatientLabs(res: CclReturnData<Lab>): {
+  error: boolean;
+  msg?: string;
+} {
+  // console.log(res.META);
+  if (!res.DATA) {
+    return { error: true, msg: "no valid data was found for the patients" };
+  }
+  if (res.DATA.length === 0) {
+    return { error: true, msg: "there was no data for the patients" };
+  }
+  // if (res.DATA.length > 1) {
+  //   return {
+  //     error: true,
+  //     msg: "more than one user was found; was expecting only one",
+  //   };
+  // }
+  return { error: false };
 }
 
 export default PatientLabs;
--- src/redux/slices/patientsSlice.tsx
+++ src/redux/slices/patientsSlice.tsx
@@ -1,5 +1,10 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Patient } from "../../data";
+import {
+  AnyAction,
+  createAsyncThunk,
+  createSlice,
+  PayloadAction,
+} from "@reduxjs/toolkit";
+import { CclReturnData, getPatientsData, Patient } from "../../data";
 
 const initialState: Patient[] = [
   {
@@ -11,6 +16,49 @@ const initialState: Patient[] = [
     NEW_TO_PANEL: 0,
   },
 ];
+
+// export const getPatientsListAsync = () => {
+//   getPatientsData(1)
+//     .then((result) => {
+//       const { error, msg } = validateGetPatients(result);
+//       if (error) {
+//         console.error("No Result");
+//         return;
+//       } else {
+//         // setUser(result.DATA[0]);
+//         return { result };
+//         // console.log(result.DATA[0]);
+//       }
+//     })
+//     .then((err) => console.error(err));
+// };
+
+export const getPatientsListAsync = createAsyncThunk(
+  "patients/getPatientsListAsync",
+  async (dispatch, getState) => {
+    return getPatientsData(1).then((result) => result.DATA);
+  }
+);
+
+function validateGetPatients(res: CclReturnData<Patient>): {
+  error: boolean;
+  msg?: string;
+} {
+  // console.log(res.META);
+  if (!res.DATA) {
+    return { error: true, msg: "no valid data was found for the patients" };
+  }
+  if (res.DATA.length === 0) {
+    return { error: true, msg: "there was no data for the patients" };
+  }
+  // if (res.DATA.length > 1) {
+  //   return {
+  //     error: true,
+  //     msg: "more than one user was found; was expecting only one",
+  //   };
+  // }
+  return { error: false };
+}
 
 export const patientsSlice = createSlice({
   name: "patient",
