8be632f5a87c3e4baaff9310696b4125c669cebd
Bugra Akdogan <akdogan.bugra@gmail.com>
2022-08-08 18:01:32 -0700

    Added todos, new ui

---
+++ src/components/Theme.tsx
@@ -0,0 +1,121 @@
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Toolbar,
+  Typography,
+  Rating,
+  Drawer,
+  Divider,
+  List,
+  Avatar,
+  Link,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import { DrawerListItem } from "./DrawerListItem";
+import { deepOrange } from "@material-ui/core/colors";
+
+export type DrawerEntry = {
+  title: string;
+  to: string;
+  icon: JSX.Element;
+};
+
+const drawerWidth = 240;
+
+function Theme({ children }: { children: React.ReactNode }) {
+  const {
+    NAME: name,
+    POSITION: position,
+    PHYSICIAN: isPhysician,
+  } = useSelector(
+    (state: RootState) =>
+      state.user.data || { NAME: "None", POSITION: "None", PHYSICIAN: "None" }
+  );
+
+  const drawerEntires: Array<DrawerEntry> = [
+    { title: "Dashboard", to: "/", icon: <DashboardIcon /> },
+    { title: "My Patients", to: "/my-patients", icon: <LocalHospitalIcon /> },
+    { title: "To-Do List", to: "/todos", icon: <FormatListNumberedIcon /> },
+    { title: "Logout", to: "/logout", icon: <PowerSettingsNewIcon /> },
+  ];
+
+  function handleLogoClick() {
+    <a href="/">Link</a>;
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }} display="flex">
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Avatar sx={{ bgcolor: deepOrange[500], marginRight: 1 }}>
+            {name.charAt(0)}
+          </Avatar>
+          <Typography variant="body2">
+            {`${name} (${position} - ${
+              isPhysician.toLowerCase() === "yes"
+                ? "Physician"
+                : "Not Physician"
+            })`}
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography variant="h5">MPAGE HUB</Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Typography variant="body2">Your Rating:</Typography>
+          <Box sx={{ display: { xs: "flex", md: "flex" } }}>
+            <Rating name="read-only" value={2} readOnly />
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            elevation: 24,
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Box
+          component="img"
+          sx={{
+            height: 64,
+            width: 64,
+            maxHeight: { xs: 233, md: 167 },
+            maxWidth: { xs: 350, md: 250 },
+            alignSelf: "center",
+          }}
+          alt="logo"
+          src="./logo192.png"
+          onClick={() => handleLogoClick()}
+        />
+        <Divider />
+        <List>
+          {drawerEntires.map(({ title, icon, to }) => (
+            <DrawerListItem title={title} icon={icon} to={to} />
+          ))}
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <div>{children}</div>
+      </Box>
+    </Box>
+  );
+}
+
+export default Theme;
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,7 +1,29 @@
-import Layout from "../components/Layout";
+import Masonry from "@mui/lab/Masonry";
+import TodosCards from "../components/TodosCards";
+import todos from "../data/json/todos.json";
 
-function Todos() {
-  return <Layout>Todos Goes Here</Layout>;
+export type todo = {
+  id: number;
+  title: string;
+  category: string;
+  description: string;
+  completed: boolean;
+};
+
+export default function Todos() {
+  return (
+    <Masonry columns={3} spacing={3}>
+      {todos.length > 0 &&
+        todos.map((todo) => (
+          <TodosCards
+            id={todo.id}
+            title={todo.title}
+            category={todo.category}
+            description={todo.description}
+            completed={todo.complete}
+          />
+        ))}
+      {todos.length === 0 && <h2>No todos found...</h2>}
+    </Masonry>
+  );
 }
-
-export default Todos;
